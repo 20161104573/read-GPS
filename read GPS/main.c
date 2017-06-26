e5887ae1fd585851da8648211e3ebf609e9d0278
20161104573 <a20161104573@admindeimac-34.local>
2017-06-26 12:09:39 +0800

    Initial Commit

---
+++ read GPS/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  read GPS
+//
+//  Created by a20161104573 on 2017/6/26.
+//  Copyright © 2017年 a20161104573. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
